967d83a252be2ebe4fc8094903c73bd233693cdf
Adrien SAUNIER <adrien.saunier@geneanet.org>
2015-08-07 12:28:34 +0200

    Reorganising Drawer directive to be able to select an area and delete

--- src/js/mr-image-drawer.js
+++ src/js/mr-image-drawer.js
@@ -4,40 +4,52 @@ app.directive('mrImageDrawer', function(){
     return {
         restrict: 'A',
         scope: {
-            rects: '=mrModel'
+            optionDelete: '=?mrAllowDelete',
+            rects: '=mrModel',
+            unit: '=mrUnit'
         },
-        template: '<div ng-repeat="rect in rects" style="' +
-            'position: absolute;' +
-            'cursor: pointer;' +
-            'top:    {{ rect.y1 }}px;' +
-            'left:   {{ rect.x1 }}px;' +
-            'width:  {{ (rect.x2 - rect.x1)}}px;' +
-            'height: {{ (rect.y2 - rect.y1)}}px;' +
-            "border: {{ rect.stroke || 3 }}px solid {{ rect.color || '#F00' }};" +
-            "background-color: {{ increaseBrightness(rect.color || '#F00', '0.3') }}" +
+        template: '<div ng-repeat="rect in rects" class="mr-face" ng-init="areaId = rect.id || $index" style="' +
+            'top:    {{ rect.y1 + unit }};' +
+            'left:   {{ rect.x1 + unit }};' +
+            'width:  {{ (rect.x2 - rect.x1) + unit}};' +
+            'height: {{ (rect.y2 - rect.y1) + unit}};' +
             '"' +
-            "ng-style='rectStyle'" +
-            "ng-mouseenter=\"rectStyle = { 'background-color': increaseBrightness(rect.color || '#F00', '0.1') }\"" +
-            "ng-mouseout=\"rectStyle =   { 'background-color': increaseBrightness(rect.color || '#F00', '0.3' ) }\"" +
-        '></div>',
+            "ng-click=\"this.toggleDisplayOptions(areaId)\""+
+            "ng-class=\"{active: this.displayArea == areaId || this.selectedArea == areaId}\""+
+            "ng-mouseover=\"this.displayInformations(areaId)\""+
+            "ng-mouseout=\"this.hideInformations(areaId)\""+
+        '>
+            <div class="mr-face-delete" ng-show="this.selectedArea == areaId && optionDelete && displayOptions" ng-click="this.delete($index)"></div>
+            <div class="mr-face-extra" ng-show="this.displayArea == areaId || this.selectedArea == areaId">
+                <h2 ng-show=\"rect.title\">{{rect.title}}</h2>
+                <p ng-show=\"rect.caption\">{{rect.caption}}</p>
+            </div>
+        </div>',
         link: function(scope, element) {
 
-            scope.increaseBrightness = function (hex, percent) {
-                // Strip the leading # if it's there
-                hex = hex.replace(/^\s*#|\s*$/g, '');
+            scope.displayInformations = function(areaID) {
+                scope.displayArea = areaID;
+            }
 
-                // Convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
-                if(hex.length == 3){
-                    hex = hex.replace(/(.)/g, '$1$1');
+            scope.hideInformations = function() {
+                scope.displayArea = null;
+            }
+
+            scope.toggleDisplayOptions = function(areaID) {
+
+                if(scope.selectedArea != areaID) {
+                    scope.selectedArea   = areaID;
+                    scope.displayOptions = true;
+                } else {
+                    scope.selectedArea   = null;
+                    scope.displayOptions = false;
                 }
+            }
 
-                var r = parseInt(hex.substr(0, 2), 16),
-                    g = parseInt(hex.substr(2, 2), 16),
-                    b = parseInt(hex.substr(4, 2), 16);
+            scope.delete = function(index) {
+                scope.rects.splice(index, 1);
+            }
 
-                return 'rgba(' + r + ',' + g + ',' + b + ',' + percent + ')';
-            };
-
-        }
+        },
     };
 });
